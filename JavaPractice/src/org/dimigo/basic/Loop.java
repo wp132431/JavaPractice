764133de199211157b6544a3b7f9662885d7b89b
wp132431 <JeongUkJae@172.17.2.99>
2015-03-27 16:34:12 +0900

    ㄴㅁㄴ

--- JavaPractice/src/org/dimigo/basic/Loop.java
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -3,12 +3,12 @@ package org.dimigo.basic;
 public class Loop {
 	public static void main(String[] args) {
 		for(int i = 0;i < 10;i++) {
-			for(int j = 0;j < i;j++)
-				System.out.print(j + 1);
-			
-			System.out.print("*");
-			for(int j = i + 1;j < 10;j++)
-				System.out.print(j + 1);
+			for(int j = 0;j < 10;j++) {
+				if(i == j)
+					System.out.print("*");
+				else
+					System.out.print(j + 1);
+			}
 			System.out.print("\n");
 		}
 	}
