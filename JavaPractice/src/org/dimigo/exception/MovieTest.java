2a2c0f75474b05d2811cc47adc9d5a85f9b007fd
JeongUkJae <jeongukjae@gmail.com>
2015-09-22 14:38:07 +0900

    asdf

--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -18,20 +18,21 @@ package org.dimigo.exception;
  */
 public class MovieTest {
 	public static void main(String[] args) {
-		try{
-			Movie[] movies = {
-				new Movie("베테랑", 15),
-				new Movie("앤트맨", 12),
-				new Movie("사도", 12)
-			};
-			
-			int age = 13;
-			
-			for(Movie movie : movies) {
+//		try{
+		Movie[] movies = {
+			new Movie("베테랑", 15),
+			new Movie("앤트맨", 12),
+			new Movie("사도", 12)
+		};
+		
+		int age = 13;
+		
+		for(Movie movie : movies) {
+			try {
 				buyTicket(movie, age);
+			} catch(Exception e) {
+				System.out.println(e.getMessage());
 			}
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
 		}
 	}
 	
