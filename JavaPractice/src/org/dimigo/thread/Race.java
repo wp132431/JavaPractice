e38ab884bc51c8038900971d51c8baff9fb6b2c4
JeongUkJae <jeongukjae@gmail.com>
2015-11-04 09:44:51 +0900

    thread edit

--- JavaPractice/src/org/dimigo/thread/Race.java
+++ JavaPractice/src/org/dimigo/thread/Race.java
@@ -26,9 +26,13 @@ public class Race {
 		System.out.println("main thread start");
 		Thread t1 = new Runner("홍길동");
 		Thread t2 = new Runner("홍길순");
+
+		t1.setPriority(Thread.MAX_PRIORITY);
+		t2.setPriority(Thread.MIN_PRIORITY);
 		
 		t1.start();
 		t2.start();
+		
 		System.out.println("main thread end");
 	}
 
